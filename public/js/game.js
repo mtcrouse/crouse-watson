e3ea43a095ee4142b86f7ccff1cd982482d64acf
mtcrouse <mettacrouse@gmail.com>
2016-11-21 15:25:51 -0800

    get platform scrolling working

--- public/js/game.js
+++ public/js/game.js
@@ -3,7 +3,6 @@
 const game = new Phaser.Game(800, 600, Phaser.CANVAS, '', { preload: preload, create: create, update: update });
 
 function preload() {
-  game.load.image('sky', 'assets/sky.png');
   game.load.image('ground', 'assets/platform.png');
   game.load.image('star', 'assets/star.png');
   game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
@@ -19,41 +18,39 @@ let score = 0;
 let scoreText;
 
 function create() {
+
     //  We're going to be using physics, so enable the Arcade Physics system
     game.physics.startSystem(Phaser.Physics.ARCADE);
 
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'sky');
+    game.stage.backgroundColor = '479cde';
 
-    //  The platforms group contains the ground and the 2 ledges we can jump on
     platforms = game.add.group();
-
-    //  We will enable physics for any object that is created in this group
     platforms.enableBody = true;
 
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
+    let ground = platforms.create(0, game.world.height - 64, 'ground');
 
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
+    //  Scale the ground to fit the width of the game (the original sprite is 400x32 in size)
     ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
     ground.body.immovable = true;
+    ground.body.velocity.y = 10;
 
     //  Now let's create two ledges
     var ledgeX1 = 400;
-    console.log(ledgeX1);
     var ledgeY1 = game.rnd.integerInRange(300,400);
-    console.log(ledgeY1);
 
     var ledge = platforms.create(ledgeX1, ledgeY1, 'ground');
     ledge.body.immovable = true;
+    ledge.body.velocity.y = -30;
 
     var ledgeX2 = -150;
     var ledgeY2 = 250;
 
     ledge = platforms.create(ledgeX2, ledgeY2, 'ground');
     ledge.body.immovable = true;
+    ledge.body.velocity.y = -30;
+
+    game.time.events.loop(3000, addRightPlatform, this);
+    game.time.events.loop(3000, addLeftPlatform, this);
 
     // The player and its settings
     player = game.add.sprite(32, game.world.height - 150, 'dude');
@@ -63,7 +60,7 @@ function create() {
 
     //  Player physics properties. Give the little guy a slight bounce.
     player.body.bounce.y = 0.2;
-    player.body.gravity.y = 200;
+    player.body.gravity.y = 400;
     player.body.collideWorldBounds = true;
 
     //  Our two animations, walking left and right.
@@ -77,10 +74,31 @@ function create() {
 
     //  Our controls.
     cursors = game.input.keyboard.createCursorKeys();
+}
 
+function addRightPlatform() {
+  let ledgeX = game.rnd.integerInRange(300,400);;
+  let ledgeY = game.rnd.integerInRange(600,650);
+
+  let ledge = platforms.create(ledgeX, ledgeY, 'ground');
+  ledge.body.immovable = true;
+  ledge.body.velocity.y = -120;
+}
+
+function addLeftPlatform() {
+  let ledgeX = game.rnd.integerInRange(-150,-200);;
+  let ledgeY = game.rnd.integerInRange(600,650);
+
+  let ledge = platforms.create(ledgeX, ledgeY, 'ground');
+  ledge.body.immovable = true;
+  ledge.body.velocity.y = -100;
 }
 
 function update() {
+
+    if (player.body.position.y >= game.world.height - player.body.height) {
+			gameOver();
+	 	}
 
     //  Collide the player and the stars with the platforms
     game.physics.arcade.collide(player, platforms);
