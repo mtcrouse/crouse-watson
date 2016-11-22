1b569d0902d54d35bf110fd890ffe6e8c73ece2d
mtcrouse <mettacrouse@gmail.com>
2016-11-22 11:33:49 -0800

    get game working with react

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = React.createClass({
   render() {
-    return <h1>Hello world</h1>;
+    return <div></div>;
   }
 });
 
--- app/js/game.js
+++
@@ -1,9 +0,0 @@
-const game = new Phaser.Game(800, 600, Phaser.CANVAS, 'gameDiv');
-
-game.state.add('boot', bootState);
-game.state.add('load', loadState);
-game.state.add('menu', menuState);
-game.state.add('main', mainState);
-game.state.add('gameOver', gameOver);
-
-game.state.start('boot');
