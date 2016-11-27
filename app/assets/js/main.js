5722e04ff185644af69b0d7e9f10694f770577a3
mtcrouse <mettacrouse@gmail.com>
2016-11-27 10:46:00 -0800

    make multiplayer optional

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -2,6 +2,7 @@ const mainState = {
 	init: function(playerNames) {
 		this.player1name = playerNames.player1;
 		this.player2name = playerNames.player2;
+		this.mode = playerNames.mode;
 	},
 
 	create: function() {
@@ -34,33 +35,40 @@ const mainState = {
 		this.player.animations.add('left', [0, 1, 2, 3], 10, true);
 		this.player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-		this.player2 = game.add.sprite(32, game.world.height - 150, 'player2');
+		this.scoreText = game.add.text(16, 16, `${this.player1name}: 0`, { fontSize: '20px', fill: '#fff' });
 
-		game.physics.arcade.enable(this.player2);
+		// Set up player 1 controls
+		this.cursors = game.input.keyboard.createCursorKeys();
 
-		this.player2.body.bounce.y = 0.2;
-		this.player2.body.gravity.y = 375;
-		this.player2.body.collideWorldBounds = true;
+		if (this.mode === 'multi') {
+			this.player2 = game.add.sprite(32, game.world.height - 150, 'player2');
 
-		this.player2.animations.add('left', [0, 1, 2, 3], 10, true);
-		this.player2.animations.add('right', [5, 6, 7, 8], 10, true);
+			game.physics.arcade.enable(this.player2);
+
+			this.player2.body.bounce.y = 0.2;
+			this.player2.body.gravity.y = 375;
+			this.player2.body.collideWorldBounds = true;
+
+			this.player2.animations.add('left', [0, 1, 2, 3], 10, true);
+			this.player2.animations.add('right', [5, 6, 7, 8], 10, true);
+
+			this.scoreText2 = game.add.text(16, 40, `${this.player2name}: 0`, { fontSize: '20px', fill: '#fff' });
+
+			// Set up player 2 controls
+			this.wasd = {
+				up: game.input.keyboard.addKey(Phaser.Keyboard.W),
+				down: game.input.keyboard.addKey(Phaser.Keyboard.S),
+				left: game.input.keyboard.addKey(Phaser.Keyboard.A),
+				right: game.input.keyboard.addKey(Phaser.Keyboard.D),
+			};
+		}
 
 		this.addStars();
 		game.time.events.loop(10000, this.addDiamond, this);
 		game.time.events.loop(7000, this.addBomb, this);
 
-		this.scoreText = game.add.text(16, 16, `${this.player1name}: 0`, { fontSize: '20px', fill: '#fff' });
-		this.scoreText2 = game.add.text(16, 40, `${this.player2name}: 0`, { fontSize: '20px', fill: '#fff' });
-		this.levelText = game.add.text(650, 16, 'Level 1', { fontSize: '20px', fill: '#fff' });
-
-		this.cursors = game.input.keyboard.createCursorKeys();
-
-		this.wasd = {
-			up: game.input.keyboard.addKey(Phaser.Keyboard.W),
-			down: game.input.keyboard.addKey(Phaser.Keyboard.S),
-			left: game.input.keyboard.addKey(Phaser.Keyboard.A),
-			right: game.input.keyboard.addKey(Phaser.Keyboard.D),
-		};
+		// Put this in when there are levels!
+		// this.levelText = game.add.text(650, 16, 'Level 1', { fontSize: '20px', fill: '#fff' });
 	},
 
 	update: function() {
@@ -74,26 +82,27 @@ const mainState = {
 			this.die(this.player);
 		}
 
-		if (this.player2.body.position.y >= game.world.height - this.player2.body.height) {
-			this.die(this.player2);
-		}
-
-		if (this.player2.body.position.y <= 0) {
-			this.die(this.player2);
-		}
-
 		game.physics.arcade.collide(this.player, this.platforms);
-		game.physics.arcade.collide(this.stars, this.platforms);
-		game.physics.arcade.collide(this.player2, this.platforms);
-
 		game.physics.arcade.overlap(this.player, this.stars, this.collectStar, null, this);
-		game.physics.arcade.overlap(this.player2, this.stars, this.collectStar, null, this);
-
 		game.physics.arcade.overlap(this.player, this.diamonds, this.collectDiamond, null, this);
-		game.physics.arcade.overlap(this.player2, this.diamonds, this.collectDiamond, null, this);
-
 		game.physics.arcade.overlap(this.player, this.bombs, this.hitBomb, null, this);
-		game.physics.arcade.overlap(this.player2, this.bombs, this.hitBomb, null, this);
+
+		if (this.mode === 'multi') {
+			if (this.player2.body.position.y >= game.world.height - this.player2.body.height) {
+				this.die(this.player2);
+			}
+
+			if (this.player2.body.position.y <= 0) {
+				this.die(this.player2);
+			}
+
+			game.physics.arcade.collide(this.player2, this.platforms);
+			game.physics.arcade.overlap(this.player2, this.stars, this.collectStar, null, this);
+			game.physics.arcade.overlap(this.player2, this.diamonds, this.collectDiamond, null, this);
+			game.physics.arcade.overlap(this.player2, this.bombs, this.hitBomb, null, this);
+		}
+
+		game.physics.arcade.collide(this.stars, this.platforms);
 
 		//  Reset the players velocity (movement)
 		this.player.body.velocity.x = 0;
@@ -119,23 +128,27 @@ const mainState = {
 			this.player.body.velocity.y = -400;
 		}
 
-		// Player 2 controls
-		this.player2.body.velocity.x = 0;
 
-		if (this.wasd.left.isDown) {
-			this.player2.body.velocity.x = -150;
-			this.player2.animations.play('left');
-		} else if (this.wasd.right.isDown) {
-			this.player2.body.velocity.x = 150;
-			this.player2.animations.play('right');
-		} else {
-			this.player2.animations.stop();
-			this.player2.frame = 4;
+		if (this.mode === 'multi') {
+			// Player 2 controls
+			this.player2.body.velocity.x = 0;
+
+			if (this.wasd.left.isDown) {
+				this.player2.body.velocity.x = -150;
+				this.player2.animations.play('left');
+			} else if (this.wasd.right.isDown) {
+				this.player2.body.velocity.x = 150;
+				this.player2.animations.play('right');
+			} else {
+				this.player2.animations.stop();
+				this.player2.frame = 4;
+			}
+
+			if (this.wasd.up.isDown && this.player2.body.touching.down) {
+				this.player2.body.velocity.y = -400;
+			}
 		}
 
-		if (this.wasd.up.isDown && this.player2.body.touching.down) {
-			this.player2.body.velocity.y = -400;
-		}
 
 		if (this.stars.countLiving() === 0) {
 			this.addStars();
@@ -172,15 +185,19 @@ const mainState = {
 			if (player === this.player) {
 				this.score += 5;
 				this.scoreText.text = `${this.player1name}: ` + this.score;
-			} else if (player === this.player2) {
-				this.score2 += 5;
-				this.scoreText2.text = `${this.player2name}: ` + this.score2;
+			} else if (this.mode === 'multi') {
+				if (player === this.player2) {
+					this.score2 += 5;
+					this.scoreText2.text = `${this.player2name}: ` + this.score2;
+				}
 			}
 
 			if (this.score >= 100) {
 				game.state.start('win', true, false, { 'winner': `${this.player1name}`, 'player1': `${this.player1name}`, 'player2': `${this.player2name}` });
-			} else if (this.score2 >= 100) {
-				game.state.start('win', true, false, { 'winner': `${this.player2name}`, 'player1': `${this.player1name}`, 'player2': `${this.player2name}` });
+			} else if (this.mode === 'multi') {
+				if (this.score2 >= 100) {
+					game.state.start('win', true, false, { 'winner': `${this.player2name}`, 'player1': `${this.player1name}`, 'player2': `${this.player2name}` });
+				}
 			}
 	},
 
@@ -202,15 +219,19 @@ const mainState = {
 		if (player === this.player) {
 			this.score += 10;
 			this.scoreText.text = `${this.player1name}: ` + this.score;
-		} else if (player === this.player2) {
-			this.score2 += 10;
-			this.scoreText2.text = `${this.player2name}: ` + this.score2;
+		} else if (this.mode === 'multi') {
+			if (player === this.player2) {
+				this.score2 += 10;
+				this.scoreText2.text = `${this.player2name}: ` + this.score2;
+			}
 		}
 
 		if (this.score >= 100) {
 			game.state.start('win', true, false, { 'winner': `${this.player1name}`, 'player1': `${this.player1name}`, 'player2': `${this.player2name}` });
-		} else if (this.score2 >= 100) {
-			game.state.start('win', true, false, { 'winner': `${this.player2name}`, 'player1': `${this.player1name}`, 'player2': `${this.player2name}` });
+		} else if (this.mode === 'multi') {
+			if (this.score2 >= 100) {
+				game.state.start('win', true, false, { 'winner': `${this.player2name}`, 'player1': `${this.player1name}`, 'player2': `${this.player2name}` });
+			}
 		}
 	},
 
@@ -230,8 +251,6 @@ const mainState = {
 			bomb.body.velocity.x = game.rnd.integerInRange(-100,-200);
 		}
 
-
-
 		bomb.checkWorldBounds = true;
 		bomb.outOfBoundsKill = true;
 	},
@@ -249,13 +268,17 @@ const mainState = {
 
 		if (player === this.player) {
 			this.scoreText.text = `${this.player1name}: DEAD`;
-		} else if (player === this.player2) {
-			this.scoreText2.text = `${this.player2name}: DEAD`;
+		} else if (this.mode === 'multi') {
+			if (player === this.player2) {
+				this.scoreText2.text = `${this.player2name}: DEAD`;
+			}
 		}
 
-		if (!this.player.alive && !this.player2.alive) {
+		if (this.mode === 'single' && !this.player.alive) {
 			this.gameOver();
-		};
+		} else if (this.mode === 'multi' && !this.player.alive && !this.player2.alive) {
+			this.gameOver();
+		}
 	},
 
 	gameOver: function() {
--- app/assets/js/menu.js
+++ app/assets/js/menu.js
@@ -5,16 +5,23 @@ const menuState = {
                               { font: '50px Arial', fill: '#ffffff' });
     const directions = game.add.text(80, 300, 'Use the arrow keys to navigate and the up arrow to jump',
                                     { font: '25px Arial', fill: '#ffffff'});
-    const starDirections = game.add.text(80, 400, 'Collect the stars to get points, but don\'t hit the bottom or top of the screen!',
+    const starDirections = game.add.text(80, 400, 'Collect the stars to get points, but don\'t hit the bottom or top of the screen',
                                     { font: '20px Arial', fill: '#ffffff'});
     const startLabel = game.add.text(80, game.world.height-80,
-                                    'Press W to start',
+                                    'Press S to play single player or M to play multiplayer',
                                     { font: '25px Arial', fill: '#ffffff'});
-    const wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
-    wKey.onDown.addOnce(this.start, this);
+    const sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
+    sKey.onDown.addOnce(this.start, this);
+
+    const mKey = game.input.keyboard.addKey(Phaser.Keyboard.M);
+    mKey.onDown.addOnce(this.startMultiplayer, this);
   },
 
   start: function() {
-    game.state.start('main', true, false, { 'player1': 'Player 1', 'player2': 'Player 2' });
+    game.state.start('main', true, false, { 'player1': 'Player 1', 'player2': 'Player 2', 'mode': 'single' });
+  },
+
+  startMultiplayer: function() {
+    game.state.start('main', true, false, { 'player1': 'Player 1', 'player2': 'Player 2', 'mode': 'multi' });
   }
 };
