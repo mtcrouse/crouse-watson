bcff86adf99d4c327a9d1af2ca70fb409ef358e3
mtcrouse <mettacrouse@gmail.com>
2016-11-27 11:36:05 -0800

    fix directions

--- app/assets/js/directions.js
+++ app/assets/js/directions.js
@@ -5,18 +5,29 @@ const directionsState = {
 
   create: function() {
     this.game.stage.backgroundColor = '479cde';
-    const name = game.add.text(80, 80, `${this.mode} Directions:`,
+
+    if (this.mode === 'single') {
+      var mode = 'Single Player';
+    } else {
+      var mode = 'Multiplayer';
+    }
+
+    const name = game.add.text(80, 100, `${mode} Directions:`,
                               { font: '50px Arial', fill: '#ffffff' });
-    const directions = game.add.text(80, 300, 'Use the arrow keys to navigate and the up arrow to jump',
-                                    { font: '25px Arial', fill: '#ffffff'});
-    const starDirections = game.add.text(80, 400, 'Collect items to get points, but don\'t hit the bottom or top of the screen',
+    const directions = game.add.text(80, 200, 'Player 1: Use the arrow keys to navigate and the up arrow to jump',
                                     { font: '20px Arial', fill: '#ffffff'});
-    const startLabel = game.add.text(80, 500,
+    if (mode === 'Multiplayer') {
+      const directions2 = game.add.text(80, 250, 'Player 2: Use A and D to move left and right and W to jump',
+                                      { font: '20px Arial', fill: '#ffffff'});
+    }
+    const starDirections = game.add.text(80, 300, 'Collect items to get points, but don\'t hit the bottom or top of the screen',
+                                    { font: '20px Arial', fill: '#ffffff'});
+    const points = game.add.text(80, 350,
                                     'Stars are worth 5 points and diamonds are worth 10 points',
-                                    { font: '25px Arial', fill: '#ffffff'});
-    const bombDirections = game.add.text(80, game.world.height-80,
-                                    'Watch out for bombs! Press W to start!',
-                                    { font: '25px Arial', fill: '#ffffff'});
+                                    { font: '20px Arial', fill: '#ffffff'});
+    const startDirections = game.add.text(80, 400,
+                                    'Press W to start!',
+                                    { font: '20px Arial', fill: '#ffffff'});
     const wKey = game.input.keyboard.addKey(Phaser.Keyboard.W);
     wKey.onDown.addOnce(this.start, this);
   },
--- app/assets/js/menu.js
+++ app/assets/js/menu.js
@@ -3,11 +3,7 @@ const menuState = {
     this.game.stage.backgroundColor = '479cde';
     const name = game.add.text(80, 80, 'Platform Game',
                               { font: '50px Arial', fill: '#ffffff' });
-    const directions = game.add.text(80, 300, 'Use the arrow keys to navigate and the up arrow to jump',
-                                    { font: '25px Arial', fill: '#ffffff'});
-    const starDirections = game.add.text(80, 400, 'Collect the stars to get points, but don\'t hit the bottom or top of the screen',
-                                    { font: '20px Arial', fill: '#ffffff'});
-    const startLabel = game.add.text(80, game.world.height-80,
+    const chooseMode = game.add.text(80, game.world.height-80,
                                     'Press S to play single player or M to play multiplayer',
                                     { font: '25px Arial', fill: '#ffffff'});
     const sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
@@ -18,10 +14,10 @@ const menuState = {
   },
 
   start: function() {
-    game.state.start('directions', true, false, { 'player1': 'Player 1', 'player2': 'Player 2', 'mode': 'single' });
+    game.state.start('directions', true, false, { 'mode': 'single' });
   },
 
   startMultiplayer: function() {
-    game.state.start('directions', true, false, { 'player1': 'Player 1', 'player2': 'Player 2', 'mode': 'multi' });
+    game.state.start('directions', true, false, { 'mode': 'multi' });
   }
 };
