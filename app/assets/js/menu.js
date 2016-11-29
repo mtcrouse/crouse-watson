15010005cc98e49a6e356f334cbd617435264a03
mtcrouse <mettacrouse@gmail.com>
2016-11-29 10:51:11 -0800

    update menu, win states

--- app/assets/js/menu.js
+++ app/assets/js/menu.js
@@ -1,18 +1,18 @@
 const menuState = {
   create: function() {
     this.game.stage.backgroundColor = '479cde';
-    const name = game.add.text(80, 80, 'Platform Game',
+    const name = this.game.add.text(80, 80, 'Platform Game',
                               { font: '50px Arial', fill: '#ffffff' });
-    const chooseMode = game.add.text(80, game.world.height-80,
+    const chooseMode = this.game.add.text(80, this.game.world.height-80,
                                     'Press S to play single player or M to play multiplayer',
                                     { font: '25px Arial', fill: '#ffffff'});
-    const sKey = game.input.keyboard.addKey(Phaser.Keyboard.S);
+    const sKey = this.game.input.keyboard.addKey(Phaser.Keyboard.S);
     sKey.onDown.addOnce(this.start, this);
     airconsole0.onMessage = function(device_id, data) {
       if (data.start) {
         if (data.start.pressed) {
           player_control_map[0].move.start = data.start.pressed;
-          game.state.start('main', true, false, { 'player1': 'Player 1', 'player2': 'Player 2', 'mode': 'multiplayer' });
+          this.game.state.start('main', true, false, { 'player1': 'Player 1', 'player2': 'Player 2', 'mode': 'multiplayer' });
         }
       }
   };
@@ -20,10 +20,10 @@ const menuState = {
   },
 
   start: function() {
-    game.state.start('directions', true, false, { 'mode': 'single' });
+    this.game.state.start('directions', true, false, { 'mode': 'single' });
   },
 
   startMultiplayer: function() {
-    game.state.start('directions', true, false, { 'mode': 'multi' });
+    this.game.state.start('directions', true, false, { 'mode': 'multi' });
   }
 };
--- app/assets/js/win.js
+++ app/assets/js/win.js
@@ -7,16 +7,16 @@ const winState = {
 
   create: function() {
     this.game.add.sprite(0, 0, 'background');
-    const name = game.add.text(80, 80, `${this.winnerName} is the winner!`,
+    const name = this.game.add.text(80, 80, `${this.winnerName} is the winner!`,
                               { font: '50px Arial', fill: '#ffffff' });
-    const startLabel = game.add.text(80, game.world.height-80,
+    const startLabel = this.game.add.text(80, this.game.world.height-80,
                                     'Press C to return to the main menu!',
                                     { font: '25px Arial', fill: '#ffffff'});
-    const cKey = game.input.keyboard.addKey(Phaser.Keyboard.C);
+    const cKey = this.game.input.keyboard.addKey(Phaser.Keyboard.C);
     cKey.onDown.addOnce(this.start, this);
   },
 
   start: function() {
-    game.state.start('boot', true, true);
+    this.game.state.start('boot', true, true);
   }
 };
