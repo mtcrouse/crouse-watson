aecd78c3fc0ceaee85936eab20864e7a0483bdda
mtcrouse <mettacrouse@gmail.com>
2016-12-02 09:13:00 -0800

    hardcode a friends table for presentation

--- app/components/Friends.jsx
+++ app/components/Friends.jsx
@@ -4,7 +4,28 @@ const Friends = React.createClass({
   render() {
     return (
       <div>
-              {this.props.currentFriends[0].id}
+        <table id="leaderboard-table">
+          <thead>
+            <tr>
+              <th>
+                USER
+              </th>
+              <th>
+                HIGH SCORE
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                Friend's name!
+              </td>
+              <td>
+                0
+              </td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
